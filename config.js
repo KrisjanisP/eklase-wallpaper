c6b9c632db54a8a7699d14f8813b895575749d1d
Krišjānis Petručeņa <krisjanispetrucena@gmail.com>
2021-03-23 13:35:00 +0200

    Revert "Start ignoring config.js"
    This reverts commit c28282787f491bd9d4d54146c1f549fff3067a4f.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.eklase = {};
 
 config.eklase.login_url = process.env.EKLASE_LOGIN_URL || 'https://my.e-klase.lv';
-config.eklase.user_name = process.env.EKLASE_USER; // Append your e-klase username like shown above.
-config.eklase.password =  process.env.EKLASE_PASSWORD; // Append your e-klase password like shown above.
+config.eklase.user_name = process.env.EKLASE_USER;
+config.eklase.password =  process.env.EKLASE_PASSWORD;
 
 module.exports = config;
